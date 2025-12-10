6187f0040ff49838c75906032c361fa96eeb28b4
andrea-croci-its <andrea.croci.studio@fitstic-edu.com>
2025-12-10 16:16:14 +0100

    Update prova.py
    added line indicator

--- prova.py
+++ prova.py
@@ -1,3 +1,3 @@
 tabellina = [x*2 for x in range(1,11)]
 output = "Hello World! Hello World! Hello World!"
-[print(output[:n]) for n in tabellina]
+[print(f"{n:>2}: {output[:n]}") for n in tabellina]
